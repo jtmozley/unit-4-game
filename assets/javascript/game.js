41e438c31f0098cd2861b2ae59fac4ef28123bc0
JTMozley <jtmozley@gmail.com>
2019-03-25 19:45:32 -0500

    removed duplicate and unnecessary js code. commented js file to explain code and functions. game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,9 +10,6 @@ var losses = 0;
 var winsText = $("#wins");
 var loseText = $("#losses");
 
-//numbers for assignment to the crystals
-//var numberOptions = [10, 5, 3, 7];
-
 //for loop creates crystals for page
 for (var i = 0; i < 4; i++) {
   var imageCrystal = $("<img>");
@@ -21,26 +18,28 @@ for (var i = 0; i < 4; i++) {
     "src",
     "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg"
   );
+  //creates random number between 1-12 for each crystal value
   imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
   $("#crystals").append(imageCrystal);
 }
 
-// This time, our click event applies to every single crystal on the page. Not just one.
+//on click add the clicked crystal value to the counter
 $(".crystal-image").on("click", function() {
   var crystalValue = $(this).attr("data-crystalvalue");
   crystalValue = parseInt(crystalValue);
-  // We then add the crystalValue to the user's "counter" which is a global variable.
-  // Every click, from every crystal adds to the global counter.
+  //adds to the global counter.
   counter += crystalValue;
 
-  // All of the same game win-lose logic applies. So the rest remains unchanged.
+  //alert user on click of total click count
   alert("New score: " + counter);
 
+  //on win add 1 to win count and reset game
   if (counter === targetNumber) {
     alert("You win!");
     wins++;
     reset();
     rNumberPrint();
+    //on lose add 1 to lose count and reset game
   } else if (counter >= targetNumber) {
     alert("You lose!!");
     losses++;
@@ -49,19 +48,20 @@ $(".crystal-image").on("click", function() {
   }
 });
 
+//generate random target number and print it and win/lose to page
 function rNumberPrint() {
   targetNumber = Math.floor(Math.random() * 101) + 19;
   $("#number-to-guess").text(targetNumber);
   winsText.text("Wins: " + wins);
   loseText.text("Losses: " + losses);
-};
+}
 
+//updates win/loss and reset counter
 function reset() {
   winsText.text("Wins: " + wins);
   loseText.text("Losses: " + losses);
   counter = 0;
-  targetNumber;
-  //place for code to randomize crystal values
-};
+}
 
+//execute function at page load
 rNumberPrint();
